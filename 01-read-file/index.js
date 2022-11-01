c499f39147706f1355cab66b5d21dd508f6a3eca
Aleksander Chumachenko <ledich19@gmail.com>
2022-11-01 12:18:14 +0300

    refactor: 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,11 @@
-const fs = require('fs')
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const pathText = path.join(__dirname,'text.txt')
-const stream = fs.createReadStream(pathText, 'utf-8');
-let data = '';
+const pathText = path.join(__dirname, "text.txt");
+const stream = fs.createReadStream(pathText, "utf-8");
+let data = "";
 
-stream.on('data', chunk => data += chunk);
-stream.on('end', () => console.log('End', data));
-stream.on('error', error => console.log('Error', error.message));
+stream
+  .on("data", (chunk) => (data += chunk))
+  .on("end", () => console.log(data))
+  .on("error", (error) => console.log("Error", error.message));
