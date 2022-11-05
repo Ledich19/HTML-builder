95bc6edc9c1c564594bf80187ef55ccd5bb87483
Aleksander Chumachenko <ledich19@gmail.com>
2022-11-05 20:48:11 +0200

    refactor: in 06-build-page builtHtml use asinc await

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,10 +4,8 @@ const {
   mkdir,
   copyFile,
   constants,
-  rmdir,
-  unlink
 } = require("fs");
-const { readdir } = require("fs/promises");
+const { readdir, readFile } = require("fs/promises");
 const path = require("path");
 
 const pathTemplate = path.join(__dirname, "template.html");
@@ -16,20 +14,18 @@ const pathDistHTML = path.join(__dirname, "project-dist", "index.html");
 const pathComponents = path.join(__dirname, "components");
 const pathStileFrom = path.join(__dirname, "styles");
 
-
-function copyFiles(from, to) {
-
-  mkdir(to, { recursive: true }, async (err) => {
+function copyFiles(pathFrom, pathTo) {
+  mkdir(pathTo, { recursive: true }, async (err) => {
     if (err) throw err;
     try {
-      const files = await readdir(from, { withFileTypes: true });
+      const files = await readdir(pathFrom, { withFileTypes: true });
       for (const file of files) {
         if (file.isDirectory()) {
-          copyFiles(path.join(from, file.name), path.join(to, file.name));
+          copyFiles(path.join(pathFrom, file.name), path.join(pathTo, file.name));
         } else {
           copyFile(
-            path.join(from, file.name),
-            path.join(to, file.name),
+            path.join(pathFrom, file.name),
+            path.join(pathTo, file.name),
             constants.COPYFILE_FICLONE,
             (err) => {
               if (err) throw err;
@@ -49,7 +45,10 @@ async function margeCss(params) {
     const arr = [];
     for (const file of files) {
       if (path.extname(file.name) === ".css") {
-        const stream = createReadStream(path.join(pathStileFrom, file.name), "utf-8");
+        const stream = createReadStream(
+          path.join(pathStileFrom, file.name),
+          "utf-8"
+        );
         let data = "";
         stream
           .on("data", (chunk) => (data += chunk))
@@ -71,29 +70,28 @@ async function margeCss(params) {
   }
 }
 
-async function findTagsAnd(html) {
+async function margeHtml(html) {
   let fileHtml = html;
   const regexp = /{{([\s\S]+?)}}/g;
-  const tags = html.match(regexp).map((s) => s.slice(2, s.length - 2));
+  const tags = await html.match(regexp).map((s) => s.slice(2, s.length - 2));
   try {
     const files = await readdir(pathComponents, { withFileTypes: true });
     for (const file of files) {
-      tags.forEach((t) => {
+      tags.forEach(async (t) => {
         if (file.name === t + ".html") {
-          let indexHTML = "";
-          const stream = createReadStream(
-            path.join(pathComponents, file.name),
-            "utf-8"
-          );
-          stream
-            .on("data", (chunk) => (indexHTML += chunk))
-            .on("end", () => {
-              fileHtml = fileHtml.replace(`{{${t}}}`, indexHTML);
-              writeFile(pathDistHTML, fileHtml, (error) => {
-                if (error) return console.error(error.message);
-              });
-            })
-            .on("error", (error) => console.log("Error", error.message));
+          try {
+            const contents = await readFile(
+              path.join(pathComponents, file.name),
+              { encoding: "utf8" }
+            );
+            fileHtml = fileHtml.replace(`{{${t}}}`, contents);
+            writeFile(pathDistHTML, fileHtml, (error) => {
+              if (error) return console.error(error.message);
+            });
+            console.log("contents");
+          } catch (err) {
+            console.error(err.message);
+          }
         }
       });
     }
@@ -102,23 +100,20 @@ async function findTagsAnd(html) {
   }
 }
 
-
 async function builtHTML() {
   let indexHTML = "";
-  
   const stream = createReadStream(pathTemplate, "utf-8");
   stream
-  .on("data", (chunk) => (indexHTML += chunk))
-  .on("end", () => {
-    
-    
-    findTagsAnd(indexHTML);
-  })
-  .on("error", (error) => console.log("Error", error.message));
+    .on("data", (chunk) => (indexHTML += chunk))
+    .on("end", () => {
+      margeHtml(indexHTML);
+    })
+    .on("error", (error) => console.log("Error", error.message));
 }
+
 builtHTML();
 copyFiles(
   path.join(__dirname, "assets"),
   path.join(pathDistBundleDir, "assets")
 );
-margeCss()
+margeCss();
